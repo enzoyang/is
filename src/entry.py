5e7d996ed0da5396cb06a21f85670d4989f1b19b
Enzo Yang <yzb2002@gmail.com>
2010-12-08 08:57:19 +0800

    调整之后的结构

--- src/entry.py
+++ src/entry.py
@@ -1,2 +1,9 @@
 #coding:UTF-8
 import web
+
+urls=()
+
+app = web.application(urls,globals())
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/settings.py
@@ -0,0 +1,4 @@
+#coding:UTF-8
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///data/infosys.db')
